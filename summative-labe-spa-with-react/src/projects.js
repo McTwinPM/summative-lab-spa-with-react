e4257565e69509da74d6ef411030690c44f7effa
Michael McCormack <mcctwin@gmail.com>
2025-07-24 15:55:24 -0500

    set up React using Vite, started ProjectCard and ProjectList

---
+++ summative-labe-spa-with-react/src/projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    title: "Project One",
+    description: "This is the first project.",
+  },
+  {
+    title: "Project Two",
+    description: "This is the second project.",
+  },
+  {
+    title: "Project Three",
+    description: "This is the third project.",
+  },
+];
