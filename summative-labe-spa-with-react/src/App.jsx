65c65632bd65487f169808cd588809578afc7ea7
Michael McCormack <mcctwin@gmail.com>
2025-07-25 18:49:05 -0500

    Filter functions

--- summative-labe-spa-with-react/src/App.jsx
+++ summative-labe-spa-with-react/src/App.jsx
@@ -12,12 +12,16 @@ function App() {
     setSearch(value);
   };
 
+  const filteredProjects = projects.filter(project =>
+    project.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="App">
       <h1>My Projects</h1>
       <p>Search for a project by name</p>
       <Filter search={search} onSearchChange={handleSearchChange} />
-      <ProjectList projects={projects} />
+      <ProjectList projects={filteredProjects} />
     </div>
 
 
