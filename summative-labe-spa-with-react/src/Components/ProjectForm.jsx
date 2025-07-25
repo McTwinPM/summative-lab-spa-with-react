c8035df1d8a8ade9493c89bbfa32e4c2c39ce33c
Michael McCormack <mcctwin@gmail.com>
2025-07-24 21:57:42 -0500

    added ProjectForm

---
+++ summative-labe-spa-with-react/src/Components/ProjectForm.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import projectCard from "../assets/projectCard.png";
+
+
+
+function ProjectForm(onAddProject) {
+    const [projectName, setProjectName] = useState("");
+    const [projectDescription, setProjectDescription] = useState("");
+
+    
+}
