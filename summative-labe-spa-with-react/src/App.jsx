81deb6930284bef09e8c6c4ebf3adf3e0400fa9b
Michael McCormack <mcctwin@gmail.com>
2025-07-25 19:47:29 -0500

    finished ProjectForm, added CSS

--- summative-labe-spa-with-react/src/App.jsx
+++ summative-labe-spa-with-react/src/App.jsx
@@ -3,13 +3,20 @@ import { useState } from 'react'
 import React from 'react'
 import ProjectList from './Components/ProjectList'
 import Filter from './Components/Filter'
-import projects from './projects'
+import initialProjects from './projects'
+import ProjectForm from './Components/ProjectForm'
 import './App.css'
 
 function App() {
   const [search, setSearch] = useState("");
+  const [projects, setProjects] = useState(initialProjects);
+  
   const handleSearchChange = (value) => {
     setSearch(value);
+  };
+
+  const handleAddProject = (newProject) => {
+    setProjects([...projects, newProject]);
   };
 
   const filteredProjects = projects.filter(project =>
@@ -19,6 +26,8 @@ function App() {
   return (
     <div className="App">
       <h1>My Projects</h1>
+      <p>Add a new project</p>
+      <ProjectForm onAddProject={handleAddProject} />
       <p>Search for a project by name</p>
       <Filter search={search} onSearchChange={handleSearchChange} />
       <ProjectList projects={filteredProjects} />
--- summative-labe-spa-with-react/src/Components/ProjectForm.jsx
+++ summative-labe-spa-with-react/src/Components/ProjectForm.jsx
@@ -1,11 +1,37 @@
 import React, { useState } from "react";
-import projectCard from "../assets/projectCard.png";
 
-
-
-function ProjectForm(onAddProject) {
+function ProjectForm({ onAddProject }) {
     const [projectName, setProjectName] = useState("");
     const [projectDescription, setProjectDescription] = useState("");
 
-    
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (projectName && projectDescription) {
+            onAddProject({ title: projectName, description: projectDescription });
+            setProjectName("");
+            setProjectDescription("");
+        }
+    }
+    if (!onAddProject) {
+        return null; // Return null if onAddProject is not provided
+    }
+
+    return (
+        <form className="project-form" onSubmit={handleSubmit}>
+            <input className="project-name-input"
+                type="text"
+                placeholder="Project Name"
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
+            />
+            <textarea className="project-description"
+                placeholder="Project Description"
+                value={projectDescription}
+                onChange={(e) => setProjectDescription(e.target.value)}
+            />
+            <button type="submit">Add Project</button>
+        </form>
+    );
 }
+
+export default ProjectForm;
